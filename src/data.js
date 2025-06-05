fd08c5133ea75783ee23c6460bb38a940ab0a517
Corpse203 <52112663+Corpse203@users.noreply.github.com>
2025-06-05 13:48:36 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,48 +1,66 @@
 const offers = [
   {
     title: "Betify",
-    bonus: "100% jusqu’à 500€",
-    description: "Cashback & Freebets hebdo",
+    bonus: " 100% offert + 30 Free Spins (en Raw Money)",
+    description: "Cashback tout les lundi",
     color: "linear-gradient(to right, #00c851, #007e33)",
     logo: "/logos/betify.jpg",
     link: "https://record.betify.partners/_xfcc71saF9DqaEn2LeVpkWNd7ZgqdRLk/1/",
-    tags: ["Crypto", "x40", "CB"],
+    tags: ["Crypto", "CB"],
   } ,
 {
   title: "Fast Slots",
-  bonus: "200%",
-  description: "Dépôt en RAW + 200% si RAW perdu + 150 Free Spins",
+  bonus: " 200% de BONUS jusqu’à 5'000€",
+  description: "10% de CASHBACK INSTANTANÉ dès la création de votre compte + 50 Freespins",
   color: "linear-gradient(to right, #7f00ff, #e100ff)",
   logo: "/logos/fastslots.png",
   link: "https://record.discasinoaffiliates.com/_LLmqEcvAbifk9mx2SfmU_WNd7ZgqdRLk/1/",
-  tags: ["Crypto", "Dépôt CB", "x30"]
+  tags: ["Crypto", "CB"]
 },
 {
   title: "Samba Slots",
-  bonus: "200%",
-  description: "Dépôt en RAW + 200% si RAW perdu + 150 Free Spins",
+  bonus: " 200% de BONUS jusqu’à 5'000€",
+  description: "10% de CASHBACK INSTANTANÉ dès la création de votre compte + 50 Freespins",,
   color: "linear-gradient(to right, #fcd34d, #fbbf24)",
   logo: "/logos/samba.png",
   link: "https://record.discasinoaffiliates.com/_LLmqEcvAbica0J_CtyaYcGNd7ZgqdRLk/1/",
-  tags: ["Crypto", "Dépôt CB", "x30"]
-},
+  tags: ["Crypto", "CB"]
+    },
 {
   title: "Golden Panda",
-  bonus: "200%",
-  description: "Dépôt en RAW + 200% si RAW perdu + 150 Free Spins",
+  bonus: " 200% de BONUS jusqu’à 5'000€",
+  description: "10% de CASHBACK INSTANTANÉ dès la création de votre compte + 50 Freespins",
   color: "linear-gradient(to right, #d4af37, #ffd700)",
   logo: "/logos/goldenpanda.png",
   link: "https://record.discasinoaffiliates.com/_LLmqEcvAbid8F8NfJ-N_DWNd7ZgqdRLk/1/",
-  tags: ["Crypto", "Dépôt CB", "x30"]
-},
+  tags: ["Crypto", "CB"]
+    },
 {
   title: "X7Casino",
-  bonus: "200%",
-  description: "Dépôt en RAW + 200% si RAW perdu + 150 Free Spins",
+  bonus: "500%",
+  description: "50 Free Spins sur Big Bass Bonanza + 10% Cashback chaque lundi",
   color: "linear-gradient(to right, #e63946, #3a86ff)",
   logo: "/logos/x7.png",
   link: "https://record.sc2707.com/_gPnXtwJDC42RpNYGCjuhmmNd7ZgqdRLk/1/?pg=4",
-  tags: ["Crypto", "Dépôt CB", "x30"]
+  tags: ["Crypto", "CB"]
+    },
+  {
+  title: "X7Casino",
+  bonus: "500%",
+  description: "50 Free Spins sur Big Bass Bonanza + 10% Cashback chaque lundi",
+  color: "linear-gradient(to right, #e63946, #3a86ff)",
+  logo: "/logos/x7.png",
+  link: "https://record.sc2707.com/_gPnXtwJDC42RpNYGCjuhmmNd7ZgqdRLk/1/?pg=4",
+  tags: ["Crypto", "CB"]
+},
+    {
+  title: "Slot",
+  bonus: "200% jusqu’à 20 000€",
+  description: "75 Free Spins en raw money + 10 Cashback tout les jours",
+  color: "linear-gradient(to right, #e63946, #3a86ff)",
+  logo: "/logos/slots.png",
+  link: "https://record.sc2707.com/_gPnXtwJDC42RpNYGCjuhmmNd7ZgqdRLk/1/?pg=4",
+  tags: ["Crypto", "CB"]
 }
 ];
 
