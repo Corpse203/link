2372b808f053cff23943c8324c8b994944b78008
Corpse203 <52112663+Corpse203@users.noreply.github.com>
2025-06-16 19:41:06 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,8 +14,7 @@ const Header = () => {
 
           {/* Desktop menu */}
           <div className="hidden md:flex space-x-8 items-center">
-            <a href="#offres" className="text-white hover:text-yellow-200 font-medium transition duration-150">Offres</a>
-            <a href="#contact" className="text-white hover:text-yellow-200 font-medium transition duration-150">Contact</a>
+
             <a
               href="https://calls-bot.onrender.com"
               target="_blank"
