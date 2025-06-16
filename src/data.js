28ab68f41a934424b8a2120fe2d9d3353b17d3c7
Corpse203 <52112663+Corpse203@users.noreply.github.com>
2025-06-16 09:59:58 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,57 +1,61 @@
 const offers = [
-  {
-    title: "Betify",
-    bonus: " 100% offert + 30 Free Spins (en Raw Money)",
-    description: "Cashback tout les lundi",
-    color: "linear-gradient(to right, #00c851, #007e33)",
-    logo: "/logos/betify.jpg",
-    link: "https://record.betify.partners/_xfcc71saF9DqaEn2LeVpkWNd7ZgqdRLk/1/",
-    tags: ["Crypto", "CB"],
-  } ,
-{
-  title: "Fast Slots",
-  bonus: " 200% de BONUS jusqu’à 5'000€",
-  description: "10% de CASHBACK INSTANTANÉ dès la création de votre compte + 50 Freespins",
-  color: "linear-gradient(to right, #7f00ff, #e100ff)",
-  logo: "/logos/fastslots.png",
-  link: "https://record.discasinoaffiliates.com/_LLmqEcvAbifk9mx2SfmU_WNd7ZgqdRLk/1/",
-  tags: ["Crypto", "CB"]
-},
-{
-  title: "Samba Slots",
-  bonus: " 200% de BONUS jusqu’à 5'000€",
-  description: "10% de CASHBACK INSTANTANÉ dès la création de votre compte + 50 Freespins",
-  color: "linear-gradient(to right, #fcd34d, #fbbf24)",
-  logo: "/logos/samba.png",
-  link: "https://record.discasinoaffiliates.com/_LLmqEcvAbica0J_CtyaYcGNd7ZgqdRLk/1/",
-  tags: ["Crypto", "CB"]
+    {
+        title: "Betify",
+        bonus: " 100% offert + 30 Free Spins (en Raw Money)",
+        description: "Jusqu'à 20% de cashback tout les lundi !",
+        color: "linear-gradient(to right, #00c851, #007e33)",
+        logo: "/logos/betify.jpg",
+        link: "https://record.betify.partners/_xfcc71saF9DqaEn2LeVpkWNd7ZgqdRLk/1/",
+        tags: ["Crypto", "CB"],
     },
-{
-  title: "Golden Panda",
-  bonus: " 200% de BONUS jusqu’à 5'000€",
-  description: "10% de CASHBACK INSTANTANÉ dès la création de votre compte + 50 Freespins",
-  color: "linear-gradient(to right, #d4af37, #ffd700)",
-  logo: "/logos/goldenpanda.png",
-  link: "https://record.discasinoaffiliates.com/_LLmqEcvAbid8F8NfJ-N_DWNd7ZgqdRLk/1/",
-  tags: ["Crypto", "CB"]
+
+    {
+        title: "Winningz",
+        bonus: "CODE : WZMEGA - 200% de bonus jusqu’à 20 000€",
+        description: "200 Free Spins offerts (50 par jour pendant 4 jours) + Jusqu'à 20% de cashback",
+        color: "linear-gradient(to right, #e63946, #3a86ff)",
+        logo: "/logos/winningz.png",
+        link: "https://bit.ly/SkrymiWinningz",
+        tags: ["Crypto", "CB"]
     },
-{
-  title: "X7Casino",
-  bonus: "500%",
-  description: "50 Free Spins sur Big Bass Bonanza + 10% Cashback chaque lundi",
-  color: "linear-gradient(to right, #e63946, #3a86ff)",
-  logo: "/logos/x7.png",
-  link: "https://record.sc2707.com/_gPnXtwJDC42RpNYGCjuhmmNd7ZgqdRLk/1/?pg=4",
-  tags: ["Crypto", "CB"]
+
+    {
+        title: "Samba Slots",
+        bonus: " 200% de BONUS jusqu’à 5'000€",
+        description: "10% de CASHBACK INSTANTANÉ dès la création de votre compte + 50 Freespins",
+        color: "linear-gradient(to right, #fcd34d, #fbbf24)",
+        logo: "/logos/samba.png",
+        link: "https://record.discasinoaffiliates.com/_LLmqEcvAbica0J_CtyaYcGNd7ZgqdRLk/1/",
+        tags: ["Crypto", "CB", "Retrait en 1h"]
     },
-  {
-  title: "Winningz",
-  bonus: "CODE : WZMEGA - 200% de bonus jusqu’à 20 000€",
-  description: "200 Free Spins offerts (50 par jour pendant 4 jours)",
-  color: "linear-gradient(to right, #e63946, #3a86ff)",
-  logo: "/logos/winningz.png",
-  link: "https://bit.ly/SkrymiWinningz",
-  tags: ["Crypto", "CB"]
+
+    {
+        title: "Fast Slots",
+        bonus: " 200% de BONUS jusqu’à 5'000€",
+        description: "10% de CASHBACK INSTANTANÉ dès la création de votre compte + 50 Freespins",
+        color: "linear-gradient(to right, #7f00ff, #e100ff)",
+        logo: "/logos/fastslots.png",
+        link: "https://record.discasinoaffiliates.com/_LLmqEcvAbifk9mx2SfmU_WNd7ZgqdRLk/1/",
+        tags: ["Crypto", "CB", "Retrait en 1h"]
+    },
+
+    {
+        title: "Golden Panda",
+        bonus: " 200% de BONUS jusqu’à 5'000€",
+        description: "10% de CASHBACK INSTANTANÉ dès la création de votre compte + 50 Freespins",
+        color: "linear-gradient(to right, #d4af37, #ffd700)",
+        logo: "/logos/goldenpanda.png",
+        link: "https://record.discasinoaffiliates.com/_LLmqEcvAbid8F8NfJ-N_DWNd7ZgqdRLk/1/",
+        tags: ["Crypto", "CB", "Retrait en 1h"]
+    },
+    {
+        title: "X7Casino",
+        bonus: "500%",
+        description: "50 Free Spins sur Big Bass Bonanza + 10% Cashback chaque lundi",
+        color: "linear-gradient(to right, #e63946, #3a86ff)",
+        logo: "/logos/x7.png",
+        link: "https://record.sc2707.com/_gPnXtwJDC42RpNYGCjuhmmNd7ZgqdRLk/1/?pg=4",
+        tags: ["Crypto", "CB"]
     }
 ];
 
